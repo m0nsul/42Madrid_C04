31f033f055012133a1a5bf8eb33970fda9b3610c
jsolano- <jsolano-@student.42madrid.com>
2021-04-19 20:54:01 +0200

    ex03 not finished

---
+++ ex02/ft_putnbr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsolano- <jsolano-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/19 19:11:06 by jsolano-          #+#    #+#             */
+/*   Updated: 2021/04/19 19:27:04 by jsolano-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	unsigned int	nbr_unsigned;
+
+	if (nb < 0)
+	{
+		nbr_unsigned = (unsigned int)(-1 * nb);
+		ft_putchar('-');
+	}
+	else
+		nbr_unsigned = (unsigned int)nb;
+	if (nbr_unsigned >= 10)
+	{
+		ft_putnbr(nbr_unsigned / 10);
+		ft_putnbr(nbr_unsigned % 10);
+	}
+	else
+		ft_putchar(nbr_unsigned + '0');
+}
